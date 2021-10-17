d69393393bd3329d3f92633a464169f7fb063d1e
Atul <atulbansal4444@gmail.com>
2021-10-17 21:45:24 +0530

    Changes: * added 3 components to see the expenses. * added 3 css for them

---
+++ first-app/src/components/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+import './componentStyling/ExpenseItem.css';
+import DateComponent from './DateComponent';
+
+function ExpenseItem(props) {
+    return (
+        <div className="expense-item">
+            <DateComponent date= {props.date} />
+            <div className="expense-item__description">
+                <h2> { props.title } </h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
---
+++ first-app/src/components/ExpensesComponent.jsx
@@ -0,0 +1,32 @@
+import './componentStyling/ExpensesComponent.css';
+import ExpenseItem from './ExpenseItem';
+
+function ExpensesComponent(props) {
+    const expenses = props.expenses;
+    return (
+        <div className = "expenses">
+            <ExpenseItem
+                title={expenses[0].title}
+                id={expenses[0].id}
+                amount={expenses[0].amount}
+                date={expenses[0].date} />
+            <ExpenseItem
+                title={expenses[1].title}
+                id={expenses[1].id}
+                amount={expenses[1].amount}
+                date={expenses[1].date} />
+            <ExpenseItem
+                title={expenses[2].title}
+                id={expenses[2].id}
+                amount={expenses[2].amount}
+                date={expenses[2].date} />
+            <ExpenseItem
+                title={expenses[3].title}
+                id={expenses[3].id}
+                amount={expenses[3].amount}
+                date={ expenses[3].date } />
+        </div>
+    );
+}
+
+export default ExpensesComponent;
