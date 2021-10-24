778d19f9378698ee3a93209a87d22a1d8633f942
Atul <atulbansal4444@gmail.com>
2021-10-24 18:46:59 +0530

    adding filter

--- first-app/src/components/Expenses/ExpensesComponent.jsx
+++ first-app/src/components/Expenses/ExpensesComponent.jsx
@@ -4,7 +4,7 @@ import ExpensesFilter from './ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
 
 const ExpensesComponent = (props) => {
-    const [year, setYear] = useState('');
+    const [year, setYear] = useState('2020');
 
     const setFilteredYear = (filteredYear) => {
         setYear(filteredYear);
@@ -15,14 +15,18 @@ const ExpensesComponent = (props) => {
             <ExpensesFilter
                 selectedYear={year}
                 onSelectYear={setFilteredYear} />
-            {props.expenses.map((expense, i) =>
-                (<ExpenseItem
-                    title={expense.title}
-                    id={expense.id}
-                    key={expense.id}
-                    amount={expense.amount}
-                    date={expense.date} />
-            ))}
+            {props.expenses.map((expense) =>
+            {
+                let refYear = expense.date.getFullYear().toString();
+                return refYear.localeCompare(year) === 0 ?
+                     <ExpenseItem
+                        title = { expense.title }
+                        id = { expense.id }
+                        key = { expense.id }
+                        amount = { expense.amount }
+                        date = { expense.date } />
+                : null;
+            })}
         </div>
     );
 }
