2355d7431e460e9f59dcc330402da1214b6ed7ac
Atul <atulbansal4444@gmail.com>
2021-10-24 19:34:04 +0530

    adding filter

--- first-app/src/components/Expenses/ExpenseListComponent.jsx
+++ first-app/src/components/Expenses/ExpenseListComponent.jsx
@@ -4,10 +4,8 @@ import '../componentStyling/ExpenseList.css';
 
 const ExpenseListComponent = (props) => {
 
-    let expensesContent = <p className='expenses-list__fallback'>No Expense Data.</p>;
-
     if (props.expenses.length === 0) {
-        return expensesContent;
+        return (<h2 className='expenses-list__fallback'>Found no expenses.</h2>);
     }
     
     return (
